2da9a47c5cc6d8a9faf2c479365d368110414fb7
julia-fu0528 <wanjia_fu@brown.edu>
2025-01-10 16:30:09 -0500

    train dir

--- python/examples/get_robot_state/train.py
+++ python/examples/get_robot_state/train.py
@@ -7,19 +7,19 @@ from pathlib import Path
 from lightning.pytorch import Trainer, seed_everything
 from lightning.pytorch.loggers import TensorBoardLogger
 from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
-
 from network import LitSpot
 from dataset import SpotDataModule
-
+print(f"hi")
+sys.exit()
 def main(num_classes, markers_path, classify, seq):
     seed_everything(42)
 
-    log_dir = os.path.join(Path(__file__).parent.parent, "gouger_logs")
+    logs_dir = os.path.join(Path(__file__).parent.parent, "gouger_logs")
 
     if classify:
-        tb_logger = TensorBoardLogger(log_dir, name="classification")
+        tb_logger = TensorBoardLogger(logs_dir, name="classification")
     else:
-        tb_logger = TensorBoardLogger(log_dir, name="regression")
+        tb_logger = TensorBoardLogger(logs_dir, name="regression")
 
     data_module = SpotDataModule(classify, seq, batch_size=128)
     if classify:
@@ -29,8 +29,8 @@ def main(num_classes, markers_path, classify, seq):
     model = LitSpot(input_dim = 24 * seq, output_dim = output_dim * seq, markers_path = markers_path, classify=classify, seq=seq)
 
 
-    checkpoint_callback = ModelCheckpoint(monitor="val_acc", mode="max", save_last=True, filename="best")
-    early_stop_callback = EarlyStopping(monitor="val_acc", patience=100, mode="max")
+    checkpoint_callback = ModelCheckpoint(dirpath = Path(__file__).parent.parent, monitor="val_acc", mode="max", save_last=True, filename="best")
+    early_stop_callback = EarlyStopping(dirpath = Path(__file__).parent.parent, monitor="val_acc", patience=100, mode="max")
 
     trainer = Trainer(
         # accelerator="gpu",
@@ -46,7 +46,8 @@ def main(num_classes, markers_path, classify, seq):
 
 
 if __name__ == "__main__":
-
+    print(f"hi")
+    os._exit(0)
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--session', type = str, required=True, help='Session for data collection')
@@ -86,6 +87,5 @@ if __name__ == "__main__":
     # euclidean_distance = np.linalg.norm(marker_10 - marker_11)
     # print(f"Euclidean distance between marker 10 and 11: {euclidean_distance}")
     # sys.exit()
-
     main(num_classes, markers_path, classify, seq)
     
